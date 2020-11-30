b5a0841bdabaf260ce87358619c7a610a1304d0d
Oxxy-k <bratello1992@gmail.com>
2020-11-30 17:28:29 +0300

    upgrade Spinner component

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -3,8 +3,8 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div class="loadingio-spinner-magnify-kz9qnycw80l">
-      <div class="ldio-dh8cmrsh2tf">
+    <div className="loadingio-spinner-magnify-kz9qnycw80l">
+      <div className="ldio-dh8cmrsh2tf">
         <div>
           <div>
             <div></div>
